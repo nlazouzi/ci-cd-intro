f89663389fb1a9302d7719decfbac3cad7f6c5e3
nlazouzi <nlazouzi@gmail.com>
2024-03-12 16:19:28 -0400

    test fixes undo

--- web/tests/routes.test.js
+++ web/tests/routes.test.js
@@ -15,7 +15,7 @@ describe('New Endpoint', () => {
       .get('/ice-flakes')
     expect(res.body).toEqual({
       resource: 'ice-flakes',
-      count: 1007,
+      count: 1005,
       shape: 'rectangle'
     })
     expect(res.statusCode).toEqual(201)
